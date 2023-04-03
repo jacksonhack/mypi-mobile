ab8e52c9d3e2acb3f446e9ecb1c2149a60e0730c
jacksonhack <56742154+jacksonhack@users.noreply.github.com>
2023-04-02 22:48:12 -0400

    point to remote API

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 // api root url
-export const API_ROOT = 'http://192.168.200.236:8000';
+export const API_ROOT = 'https://cherry-crumble-93623.herokuapp.com';
 
 // mock API root url
 export const MOCK_API_ROOT = 'https://c02d777b-4452-4d0a-8679-2a9695b7494f.mock.pstmn.io';
